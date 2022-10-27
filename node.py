255b81f49608e112151adfc968445021c6b72d7b
yanxinhan98 <114435563+yanxinhan98@users.noreply.github.com>
2022-10-27 11:37:05 -0500

    Update node.py

--- node.py
+++ node.py
@@ -1,6 +1,9 @@
 class Node:
   def __init__(self):
+    # Class label 
     self.label = None
+    # attribute name for division
     self.attr = None 
+    # child node are stored as key, value pair, while key is the attribute value and value is the child node 
     self.children = {}
 	# you may want to add additional fields here...
